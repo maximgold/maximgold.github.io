731ba6d1d60688d0e5d110f39394ea39d79c62f2
maximgold <johnpk5525@gmail.com>
2022-02-17 00:42:18 +0900

    .

--- Vanila_JS/Assignment/Guessing game/guess.js
+++ Vanila_JS/Assignment/Guessing game/guess.js
@@ -1,3 +1,30 @@
-const getNumber = document.querySelector("#input_number input");
+const setNumber = document.querySelector("#inputSetNum");
+const getGuess = document.querySelector("#inputGuessNum");
+const getSubmit = document.querySelector("#set_number");
 
-console.log(getNumber);
+const displayNum = document.querySelector("#display span");
+const displayResult = document.querySelector("#display span:last-child");
+
+console.log(setNumber);
+console.log(getGuess);
+console.log(getSubmit);
+
+
+function handleTodoSubmit (event) {
+    event.preventDefault();
+    const setNum = setNumber.value;
+    const guessNum = getGuess.value;
+    const genNum = Math.ceil(Math.random() * setNum);
+
+    displayNum.innerText = `You chose: ${guessNum}, the machine chose: ${genNum}`;
+    
+    if (genNum == guessNum) {    
+        displayResult.innerText = "You Win!"
+    } else {
+        displayResult.innerText = "You Lost!"
+    };
+ 
+}
+
+
+getSubmit.addEventListener("submit", handleTodoSubmit);
