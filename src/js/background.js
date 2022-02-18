7786efb910d325b64e5e0aaad726eda5a8e76b16
maximgold <johnpk5525@gmail.com>
2022-02-19 02:23:31 +0900

    Update background.js
    add src path

--- src/js/background.js
+++ src/js/background.js
@@ -8,5 +8,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `src/img/${chosenImage}`;
 document.body.appendChild(bgImage);
